c867fdf2ec7daa442a2eac3d2c48c57ff53543b6
Storm <stormmayer@gmail.com>
2019-10-03 21:29:13 -0400

    Began Player script and built unit prefabs.

---
+++ Deliver/Assets/Scripts/CameraDolly.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraDolly : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //Check input
+    void readInput()
+    {
+
+    }
+}
---
+++ Deliver/Assets/Scripts/Player_Unit.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//This script will act as the parent to all player unit types.
+public class Player_Unit : MonoBehaviour
+{
+    //Attributes
+    bool isSelected;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isSelected = false;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //Unit selection
+    void checkSelection()
+    {
+        //this.GetComponent<BoxCollider>();
+    }
+
+    private void OnMouseDown()
+    {
+        Debug.Log("This is a friendly unit");
+        isSelected = true;
+    }
+}
